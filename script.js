6d1d3fc5e9a5493f47bba95ecc4e2e53cb9a0b5e
Muna Bhandari <hotline.muna@gmail.com>
2021-03-23 16:07:59 +0545

    Traffic light automation completed

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
-function trafficLightCycle(){
-    
+function trafficLightCycle() {
+
+    var greenLight = document.querySelector('.green');
+    var yellowLight = document.querySelector('.yellow');
+    var redLight = document.querySelector('.red');
+
+    setTimeout(function () {
+        greenLight.classList.add('on')
+        greenLight.classList.remove('off')
+    }, 0);
+
+
+    setTimeout(function () {
+        greenLight.classList.add('off')
+        greenLight.classList.remove('on')
+    }, 2000);
+
+    setTimeout(function () {
+        yellowLight.classList.add('on')
+        yellowLight.classList.remove('off')
+    }, 2000);
+
+
+    setTimeout(function () {
+        yellowLight.classList.add('off')
+        yellowLight.classList.remove('on')
+    }, 4000);
+
+    setTimeout(function () {
+        redLight.classList.add('on')
+        redLight.classList.remove('off')
+    }, 4000);
+
+
+    setTimeout(function () {
+        redLight.classList.add('off')
+        redLight.classList.remove('on')
+    }, 6000);
+
+
 }
+
+trafficLightCycle();
+setInterval(trafficLightCycle, 6000);
